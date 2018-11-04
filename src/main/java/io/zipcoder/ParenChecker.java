47aef220ebf7c19304bde8dd39c4d8cf6465a15b
Soujanya Buragapu <soujanyaburagapu@zipcoders-MacBook-Pro-35.local>
2018-11-04 01:02:10 -0500

    EncapsulationCharacter

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,38 @@
 package io.zipcoder;
 
-public class ParenChecker {
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class ParenChecker
+{
+    private static final String OPEN = "([{";
+    private static final String CLOSE = ")]}";
+    public static boolean isBalanced(String expression)
+    {
+        Stack<Character> stack = new Stack<Character>();
+        boolean balanced = true;
+        try {
+            int index = 0;
+            while (balanced && index < expression.length()) {
+                char nextCh = expression.charAt(index);
+                if (isOpen(nextCh)) {
+                    stack.push(nextCh);
+                } else if (isClose(nextCh)) {
+                    char topCh = stack.pop();
+                    balanced =
+                            OPEN.indexOf(topCh) == CLOSE.indexOf(nextCh);
+                }
+                index++;
+            }
+        } catch (EmptyStackException ex) {
+            balanced = false;
+        }
+        return (balanced && stack.empty());
+    }
+    private static boolean isOpen(char ch) {
+        return OPEN.indexOf(ch) > -1;
+    }
+    private static boolean isClose(char ch) {
+        return CLOSE.indexOf(ch) > -1;
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,26 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ParenCheckerTest {
+public class ParenCheckerTest
+{
+    @Test
+    public void testparenchecker()
+    {
+        ParenChecker p=new ParenChecker();
+        String str="welcome[to] the {Zipcode";
+        boolean expected= false;
+        boolean actual=p.isBalanced(str) ;
+        Assert.assertEquals(expected,actual) ;
+    }
+
+    @Test
+    public void testparenchecker1()
+    {
+        ParenChecker p=new ParenChecker();
+        String str="welcome[to] the {Zipcode}";
+        boolean expected= true;
+        boolean actual=p.isBalanced(str) ;
+        Assert.assertEquals(expected,actual) ;
+    }
 
 }
